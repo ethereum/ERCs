0a2278563d2abc2cb7b54522a21aff3d0eed94ba
kopy-kat <kopp.konrad@gmail.com>
2024-01-31 14:22:27 +0000

    feat: implement feedback and update interface file

--- assets/erc-7579/IMSA.sol
+++ assets/erc-7579/IMSA.sol
@@ -1,12 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.21;
 
-struct Execution {
-    address target;
-    uint256 value;
-    bytes callData;
-}
-
+/// ERC-4337 (v0.7) UserOperation struct
 struct UserOperation {
     address sender;
     uint256 nonce;
@@ -35,36 +30,24 @@ interface IERC7579Account {
     /**
      * @dev Executes a transaction on behalf of the account.
      *         This function is intended to be called by ERC-4337 EntryPoint.sol
-     * @dev Ensure adequate authorization control: i.e. onlyEntryPointOrSelf
-     *
-     * @dev MSA MUST implement this function signature.
-     * If a mode is requested that is not supported by the Account, it MUST revert
      * @param mode The encoded execution mode of the transaction. See ModeLib.sol for details
-     * @param executionCalldata The return data of the executed contract call.
+     * @param executionCalldata The encoded execution call data
+     *
+     * MUST ensure adequate authorization control: i.e. onlyEntryPointOrSelf
+     * If a mode is requested that is not supported by the Account, it MUST revert
      */
     function execute(bytes32 mode, bytes calldata executionCalldata) external payable;
 
     /**
      * @dev Executes a transaction on behalf of the account.
      *         This function is intended to be called by Executor Modules
-     * @dev Ensure adequate authorization control: i.e. onlyExecutorModule
-     *
-     * @dev MSA MUST implement this function signature.
-     * If a mode is requested that is not supported by the Account, it MUST revert
      * @param mode The encoded execution mode of the transaction. See ModeLib.sol for details
-     * @param executionCalldata The return data of the executed contract call.
+     * @param executionCalldata The encoded execution call data
+     *
+     * MUST ensure adequate authorization control: i.e. onlyExecutorModule
+     * If a mode is requested that is not supported by the Account, it MUST revert
      */
     function executeFromExecutor(bytes32 mode, bytes calldata executionCalldata) external payable;
-
-    /**
-     * @dev ERC-4337 executeUserOp according to ERC-4337 v0.7
-     *         This function is intended to be called by ERC-4337 EntryPoint.sol
-     * @dev Ensure adequate authorization control: i.e. onlyEntryPointOrSelf
-     *      The implementation of the function is OPTIONAL
-     *
-     * @param userOp PackedUserOperation struct (see ERC-4337 v0.7+)
-     */
-    function executeUserOp(UserOperation calldata userOp) external payable;
 
     /**
      * @dev ERC-4337 validateUserOp according to ERC-4337 v0.7
@@ -81,64 +64,78 @@ interface IERC7579Account {
         returns (uint256 validSignature);
 
     /**
-     * @dev installs a Module of a certain type on the smart account
-     * @dev Implement Authorization control of your chosing
-     * @param moduleType the module type ID according the ERC-7579 spec
-     * @param module the module address
-     * @param initData arbitrary data that may be required on the module during `onInstall`
-     * initialization.
+     * @dev Initializes the account. Function might be called directly or by a factory
+     * @param data encoded data that can be used during the initialization phase
+     *
+     * MUST revert if the account is already initialized
      */
-    function installModule(uint256 moduleType, address module, bytes calldata initData) external payable;
+    function initializeAccount(bytes calldata data) external payable;
 
     /**
-     * @dev uninstalls a Module of a certain type on the smart account
-     * @dev Implement Authorization control of your chosing
-     * @param moduleType the module type ID according the ERC-7579 spec
-     * @param module the module address
-     * @param deInitData arbitrary data that may be required on the module during `onInstall`
-     * initialization.
+     * @dev Returns the account id of the smart account
+     * @return accountImplementationId the account id of the smart account
+     *
+     * MUST return a non-empty string
+     * The accountId SHOULD be structured like so:
+     *        "vendorname.accountname.semver"
+     * The id SHOULD be unique across all smart accounts
      */
-    function uninstallModule(uint256 moduleType, address module, bytes calldata deInitData) external payable;
+    function accountId() external view returns (string memory accountImplementationId);
 
     /**
-     * Function to check if the account supports a certain CallType or ExecType (see ModeLib.sol)
+     * @dev Function to check if the account supports a certain execution mode (see above)
      * @param encodedMode the encoded mode
+     *
+     * MUST return true if the account supports the mode and false otherwise
      */
     function supportsAccountMode(bytes32 encodedMode) external view returns (bool);
 
     /**
-     * Function to check if the account supports installation of a certain module type Id
-     * @param moduleTypeId the module type ID according the ERC-7579 spec
+     * @dev Function to check if the account supports a certain module typeId
+     * @param moduleTypeId the module type ID according to the ERC-7579 spec
+     *
+     * MUST return true if the account supports the module type and false otherwise
      */
     function supportsModule(uint256 moduleTypeId) external view returns (bool);
 
     /**
-     * Function to check if the account has a certain module installed
-     * @param moduleType the module type ID according the ERC-7579 spec
-     *      Note: keep in mind that some contracts can be multiple module types at the same time. It
-     *            thus may be necessary to query multiple module types
+     * @dev Installs a Module of a certain type on the smart account
+     * @param moduleType the module type ID according to the ERC-7579 spec
      * @param module the module address
-     * @param additionalContext additional context data that the smart account may interpret to
-     *                          identifiy conditions under which the module is installed.
-     *                          usually this is not necessary, but for some special hooks that
-     *                          are stored in mappings, this param might be needed
+     * @param initData arbitrary data that may be required on the module during `onInstall`
+     * initialization.
+     *
+     * MUST implement authorization control
+     * MUST call `onInstall` on the module with the `initData` parameter if provided
+     * MUST emit ModuleInstalled event
+     * MUST revert if the module is already installed or the initialization on the module failed
+     */
+    function installModule(uint256 moduleType, address module, bytes calldata initData) external payable;
+
+    /**
+     * @dev Uninstalls a Module of a certain type on the smart account
+     * @param moduleType the module type ID according the ERC-7579 spec
+     * @param module the module address
+     * @param deInitData arbitrary data that may be required on the module during `onInstall`
+     * initialization.
+     *
+     * MUST implement authorization control
+     * MUST call `onUninstall` on the module with the `deInitData` parameter if provided
+     * MUST emit ModuleUninstalled event
+     * MUST revert if the module is not installed or the deInitialization on the module failed
+     */
+    function uninstallModule(uint256 moduleType, address module, bytes calldata deInitData) external payable;
+
+    /**
+     * @dev Returns whether a module is installed on the smart account
+     * @param moduleType the module type ID according the ERC-7579 spec
+     * @param module the module address
+     * @param additionalContext arbitrary data that may be required to determine if the module is installed
+     *
+     * MUST return true if the module is installed and false otherwise
      */
     function isModuleInstalled(uint256 moduleType, address module, bytes calldata additionalContext)
         external
         view
         returns (bool);
-
-    /**
-     * @dev Returns the account id of the smart account
-     * @return accountImplementationId the account id of the smart account
-     * the accountId should be structured like so:
-     *        "vendorname.accountname.semver"
-     */
-    function accountId() external view returns (string memory accountImplementationId);
-
-    /**
-     * @dev initialized the account. Function might be called directly, or by a Factory
-     * @param data. encoded data that can be used during the initialization phase
-     */
-    function initializeAccount(bytes calldata data) external payable;
 }
