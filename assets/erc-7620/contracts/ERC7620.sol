8da3cdb1866d3b9638bf58eed204333747904090
liaoyuan <liaoyuan@outlook.com>
2024-02-12 22:39:03 +0800

    re-license

--- assets/erc-7620/contracts/ERC7620.sol
+++ assets/erc-7620/contracts/ERC7620.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: CC0-1.0
+
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
--- assets/erc-7620/contracts/IERC7620.sol
+++ assets/erc-7620/contracts/IERC7620.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: CC0-1.0
+
 pragma solidity ^0.8.20;
 
 interface IERC7620 {
