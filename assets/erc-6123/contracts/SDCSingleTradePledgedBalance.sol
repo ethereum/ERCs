d0b6b6450f91be7ea0022c5598b4b7ed025c1c85
Christian Fries <email@christian-fries.de>
2024-08-01 10:55:07 +0200

    Refactor rename SDC.sol to SDCSingleTrade.sol since it is the state maschine of a single trade only.

--- assets/erc-6123/contracts/SDCSingleTradePledgedBalance.sol
+++ assets/erc-6123/contracts/SDCSingleTradePledgedBalance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity >=0.8.0 <0.9.0;
 
-import "./SDC.sol";
+import "./SDCSingleTrade.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./ERC20Settlement.sol";
