7b5f7a1f81166c34f8e13202cfb2c3423cdcc1e0
frank <frank@frankdeMacBook-Pro.local>
2024-12-16 17:26:45 +0800

    erc7765

--- assets/erc-7765/contracts/ERC7765Example.sol
+++ assets/erc-7765/contracts/ERC7765Example.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
--- assets/erc-7765/contracts/interfaces/IERC7765.sol
+++ assets/erc-7765/contracts/interfaces/IERC7765.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
 /// @title ERC-7765 Privileged Non-Fungible Tokens Tied To Real World Assets
