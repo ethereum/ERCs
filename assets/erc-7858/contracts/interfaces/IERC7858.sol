8be1b838e4f3e7d3aa012ebd004a6a6ab33af76f
Sam Wilson <57262657+SamWilsn@users.noreply.github.com>
2025-01-09 10:47:23 -0500

    Update and rename IERCXXXX.sol to IERC7858.sol

--- assets/erc-7858/contracts/interfaces/IERC7858.sol
+++ assets/erc-7858/contracts/interfaces/IERC7858.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-interface IERCXXXX {
+interface IERC7858 {
     enum EXPIRY_TYPE {
         BLOCK_BASED, // block.number
         TIME_BASED // block.timestamp
