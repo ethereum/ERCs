0b5affab8640e842f1dfeeac9da78de5fa0b5bf7
Christian Fries <email@christian-fries.de>
2025-11-16 12:51:59 +0100

    Splitting transferAndDecrypt into confirmTransfer and transferAndDecrypt to support Multi-Party-DvP.

--- assets/erc-7573/contracts/IDecryptionContract.sol
+++ assets/erc-7573/contracts/IDecryptionContract.sol
@@ -41,6 +41,17 @@ interface IDecryptionContract {
     event TransferIncepted(uint256 id, int amount, address from, address to, string keyEncryptedSuccess, string keyEncryptedFailure);
 
     /**
+     * @dev Emitted when the transfer is confirmed.
+     * @param id the trade identifier of the trade.
+     * @param amount the amount to be transferred.
+     * @param from The address of the sender of the payment.
+     * @param to The address of the receiver of the payment.
+     * @param keyEncryptedSuccess Encryption of the key that is emitted upon success.
+     * @param keyEncryptedFailure Encryption of the key that is emitted upon failure.
+     */
+    event TransferConfirmed(uint256 id, int amount, address from, address to, string keyEncryptedSuccess, string keyEncryptedFailure);
+
+    /**
      * @dev Emitted when a transfer has been performed with a success or failure.
      * @param sender a sender. May provide information of the origin of this request.
      * @param id the trade ID.
@@ -71,15 +82,24 @@ interface IDecryptionContract {
     function inceptTransfer(uint256 id, int amount, address from, string memory keyEncryptedSuccess, string memory keyEncryptedFailure) external;
 
     /**
-     * @notice Called from the sender of the amount to initiate completion of the payment transfer.
-     * @dev emits a {TransferKeyRequested} with keys depending on completion success.
+     * @notice Called by the sender of the amount to confirm the payment transfer.
+     * @dev emits a {TransferConfirmed}
      * @param id the trade identifier of the trade.
      * @param amount the amount to be transferred.
      * @param to The address of the receiver of the payment. Note: the sender of the payment (from) is implicitly the message.sender.
      * @param keyEncryptedSuccess Encryption of the key that is emitted upon success.
      * @param keyEncryptedFailure Encryption of the key that is emitted upon failure.
      */
-    function transferAndDecrypt(uint256 id, int amount, address to, string memory keyEncryptedSuccess, string memory keyEncryptedFailure) external;
+    function confirmTransfer(uint256 id, int amount, address to, string memory keyEncryptedSuccess, string memory keyEncryptedFailure) external;
+
+    /**
+     * @notice Called by the sender of (first) confirmTransfer initiate completion of the payment transfer(s).
+     *   Note: In case of a multi party DvP there may be multiple incept/confirm pays.
+     *   In case of single DvP the previous confirmTransfer may directly call transferAndDecrypt.
+     * @dev emits a {TransferKeyRequested} with keys depending on completion success.
+     * @param id the trade identifier of the trade.
+     */
+    function transferAndDecrypt(uint256 id) external;
 
     /**
      * @notice Called from the receiver of the amount to cancel payment transfer (cancels the incept transfer).
