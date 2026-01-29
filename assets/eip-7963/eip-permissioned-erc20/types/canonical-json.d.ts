a50350a584e3970720da8d0700489bb359df9f9a
chadxeth <junmtan.eth@gmail.com>
2026-01-29 10:37:43 +0800

    Reduce reference implementation to core contracts only

--- assets/eip-7963/eip-permissioned-erc20/types/canonical-json.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'canonical-json' {
-  // Declare the type of the default export 'canonicalize'
-  // It takes any JS value and returns a string.
-  function canonicalize(obj: unknown): string;
-  // Use export default for default export
-  export default canonicalize;
-} 
